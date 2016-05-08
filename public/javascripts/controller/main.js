b9b4aff4e3d2dc1e71c7957f23916f4b65ecfd2e
GuroKung <greenblood11@hotmail.com>
2016-05-09 02:57:47 +0700

    fix timer bug

--- public/javascripts/controller/main.js
+++ public/javascripts/controller/main.js
@@ -7,6 +7,26 @@ angular.module('ku-regis', ['ui.router', 'ngCookies'])
     if (!Auth.isLogin()) {
       $state.go('login', {}, {reload: true})
     }
+
+    // start timer
+    startTime()
+
+    function startTime () {
+      var today = new Date()
+      var h = today.getHours()
+      var m = today.getMinutes()
+      var s = today.getSeconds()
+      m = checkTime(m)
+      s = checkTime(s)
+      document.getElementById('time').innerHTML =
+        h + ':' + m + ':' + s
+      var t = setTimeout(startTime, 500)
+    }
+    function checkTime (i) {
+      if (i < 10) {i = '0' + i}; // add zero in front of numbers < 10
+      return i
+    }
+
   })
 
   .controller('NavbarController', function ($state, Auth) {
